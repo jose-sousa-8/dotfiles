4eda712640477ef23db20a2f57705cb94742a6f7
José Sousa <zpedro_s@hotmail.com>
2023-10-16 21:29:48 +0100

    feat: nvim code completion

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -52,6 +52,16 @@ return packer.startup(function(use)
   
   use({ 'rose-pine/neovim', as = 'rose-pine' })
  
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
